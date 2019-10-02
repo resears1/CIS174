3782956bde1bfb3facd8f14f3412b6e3e3b1794e
MrMuffinz <Sgtmuffin126@hotmail.com>
2019-10-02 18:50:25 -0500

    Assignment 6.1

---
+++ CIS174_TestCoreApp/CIS174_TestCoreApp/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIS174_TestCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+    public class StudentController : Controller
+    {
+       public IActionResult Index()
+        {
+            var viewModel = new StudentViewModel
+            {
+                AccessLevel = 5,
+                FirstName = "Ricky",
+                LastName = "Sears",
+                Grade = 'B'
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ CIS174_TestCoreApp/CIS174_TestCoreApp/Models/StudentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class StudentViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Grade { get; set; }
+        public int AccessLevel { get; set; }
+    }
+}
